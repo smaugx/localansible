ba56544603bf80ced067d76fb74c4a137f4436a4
Smaug X <linuxcode2niki@gmail.com>
2019-12-01 23:00:58 +0800

    Update localansible.py
    udpate

--- localansible.py
+++ localansible.py
@@ -3,6 +3,8 @@
 
 import os
 import sys
+import commands
+import subprocess
 
 
 
@@ -23,10 +25,24 @@ def run(cmd, base = './'):
     for path in abs_path_list:
         new_cmd = 'cd {0} && {1} '.format(path, cmd)
         std_print = '\n$ {0}'.format(new_cmd)
-        #print("\033[0;31;40m\t{0}\033[0m".format(std_print))
+
+
+        #result = os.popen(new_cmd).readlines()
+
+        #result = commands.getoutput(new_cmd)
+
+        result = None
+        try:
+            result = subprocess.check_output(new_cmd, shell=True)
+            std_print += ' CHANGED'
+        except Exception as e:
+            std_print += ' FAILED'
+
         print("\033[4;32;47m{0}\033[0m".format(std_print))
-        #os.system(new_cmd)
-        result = os.popen(new_cmd).readlines()
+        if not result:
+            continue
+
+        result = result.split('\n')
         for r in result:
             if r.endswith('\n'):
                 r = r[:-1]
@@ -41,6 +57,6 @@ if __name__ == '__main__':
     if len(sys.argv) == 3:
         base = sys.argv[1]
         cmd  = sys.argv[2]
-    print('base: {0} cmd: {1}'.format(base, cmd))
+    #print('base: {0} cmd: {1}'.format(base, cmd))
 
     run(cmd, base)
